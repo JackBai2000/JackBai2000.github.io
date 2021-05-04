a2e8aefab465faca37576b6c7dfb7c1b630fb42e
Jack Bai <jackybai2000@hotmail.com>
2021-05-04 13:33:19 -0400

    Starting from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom"
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>"Hello I am trying to make something here."</h1>
+            </div>
+        );
+    }
+};
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
